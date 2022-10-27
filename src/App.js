59edf7cef94a06040ffa182e4cc68430392d20e4
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-10-27 17:14:31 +0100

    landing page refactored into it's home component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import welcome from './images/Welcome.png';
+import Home from './views/Home';
+
 function App() {
   return (
     <div className="App">
-      <div className="welcome">
-        <img src={welcome} alt="welcome" className="welcome__img" />
-      </div>
-
-      <div className="welcome-text">
-        <h1 className="welcome-text__heading">
-          Discover the Weather in Your City
-        </h1>
-        <p className="welcome-text__paragraph">
-          Get to know your weather maps and <br /> radar precipitation forecast
-        </p>
-      </div>
-
-      <button className="cta">Get Started</button>
+      <Home />
     </div>
   );
 }
---
+++ src/views/Home.js
@@ -0,0 +1,24 @@
+import welcome from '../images/Welcome.png';
+
+const Home = () => {
+  return (
+    <>
+      <div className="welcome">
+        <img src={welcome} alt="welcome" className="welcome__img" />
+      </div>
+
+      <div className="welcome-text">
+        <h1 className="welcome-text__heading">
+          Discover the Weather in Your City
+        </h1>
+        <p className="welcome-text__paragraph">
+          Get to know your weather maps and <br /> radar precipitation forecast
+        </p>
+      </div>
+
+      <button className="cta">Get Started</button>
+    </>
+  );
+};
+
+export default Home;
