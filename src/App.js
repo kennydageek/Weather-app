87f5fa2eca5d1139d8c4891700282389acfb3733
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-10-27 16:00:14 +0100

    landing page for the weather app done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import './App.css';
-
+import welcome from './images/Welcome.png';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="welcome">
+        <img src={welcome} alt="welcome" className="welcome__img" />
+      </div>
+
+      <div className="welcome-text">
+        <h1 className="welcome-text__heading">
+          Discover the Weather in Your City
+        </h1>
+        <p className="welcome-text__paragraph">
+          Get to know your weather maps and <br /> radar precipitation forecast
+        </p>
+      </div>
+
+      <button className="cta">Get Started</button>
+    </div>
+  );
 }
 
 export default App;
