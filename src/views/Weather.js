7b6db4e13f36beae0fe3d7bc05f30d0931c579c0
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-10-28 13:18:19 +0100

    additional info added

--- src/views/Weather.js
+++ src/views/Weather.js
@@ -2,10 +2,12 @@ import { MdOutlineLightMode, MdOutlineDarkMode } from 'react-icons/md';
 import { IconContext } from 'react-icons';
 import { useState } from 'react';
 import Skycons, { SkyconsType } from 'react-skycons';
-// import useFetch from '../useFetch';
+import useFetch from '../useFetch';
 
 const Weather = () => {
   const [mode, setMode] = useState(true);
+
+  useFetch('Lagos');
 
   const handleToggle = () => {
     setMode(!mode);
@@ -35,7 +37,7 @@ const Weather = () => {
         </div>
       </nav>
 
-      <h2 class="heading">Today's Report</h2>
+      <h2 className="heading">Today's Report</h2>
 
       <div className="weather-icon">
         <Skycons
@@ -53,6 +55,47 @@ const Weather = () => {
         <div className="weather-temp">
           <p className="weather-temp__number">29</p>
           <p className="weather-temp__degree">O</p>
+        </div>
+      </div>
+
+      <div className="info-add">
+        <div className="wind-container">
+          <Skycons
+            color="#eda329"
+            type={SkyconsType.WIND}
+            animate={false}
+            size={32}
+            resizeClear={true}
+            {...svgProps}
+          />
+          <p className="wind-speed">23 km/h</p>
+          <p className="wind-footer">Wind</p>
+        </div>
+
+        <div className="wind-container">
+          <Skycons
+            color="#eda329"
+            type={SkyconsType.SLEET}
+            animate={false}
+            size={32}
+            resizeClear={true}
+            {...svgProps}
+          />
+          <p className="wind-speed">23 m/s</p>
+          <p className="wind-footer">Humidity</p>
+        </div>
+
+        <div className="wind-container">
+          <Skycons
+            color="#eda329"
+            type={SkyconsType.CLEAR_DAY}
+            animate={false}
+            size={32}
+            resizeClear={true}
+            {...svgProps}
+          />
+          <p className="wind-speed">23 Pa</p>
+          <p className="wind-footer">Pressure</p>
         </div>
       </div>
     </IconContext.Provider>
